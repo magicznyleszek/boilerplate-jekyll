79d815795e1d7c249d731ccbb7f732088b55d452
Leszek <smutnyleszek@gmail.com>
2014-03-28 15:31:42 +0100

    added font for code tag, universalized the code typography, some cleanups

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 function projectInterface() {
+    // --------------------------------------------------
     // declare a public run function
+    // with all private stuff initialized inside of it
+    // --------------------------------------------------
+
     this.run = run;
     function run() {
-        // initialize all functions here
         doSomething();
     }
+
 
     // --------------------------------------------------
     // this does something because something
     // --------------------------------------------------
+
     function doSomething() {
         if(true === true) {
             console.log("truth");
         }
     }
 }
-// create a public object
+
+// --------------------------------------------------
+// create a public object and start its run function
+// --------------------------------------------------
+
 var knowYourDinosaurs = new projectInterface();
 // run functions
 knowYourDinosaurs.run();
